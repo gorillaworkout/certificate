43be887fe83f14b793c6f83fd22b9be85c894c12
Bayu Darmawan <darmawanbayu1@gmail.com>
2023-05-25 23:17:51 +0700

    update

--- src/app/blog/[img]/[badge]/[content]/[description]/[issue]/page.jsx
+++ src/app/blog/[img]/[badge]/[content]/[description]/[issue]/page.jsx
@@ -6,26 +6,25 @@ import Head from 'next/head';
 
 export async function generateMetadata({ params }) {
     // const product = await getProduct(params.id);
-    console.log(params.id,'params id')
-    console.log(decodeURIComponent(params.id),'params id')
+    console.log(params.img,'params id')
+    console.log(decodeURIComponent(params.img),'params id')
     return { 
-        title: params.id ,
+        title: params.img ,
         twitter:{
             card:'summary_large_image',
-            title:decodeURIComponent(params.description.split('|')[0]),
-            description:decodeURIComponent(params.description.split('|')[0]),
+            title:decodeURIComponent(params.badge),
+            description:decodeURIComponent(params.description),
             siteId:'12971917292',
             creator:'Gorillaworkoutjs',
             creatorId:'asdasdasdasd',
-            images:decodeURIComponent(params.id)
+            images:decodeURIComponent(params.img)
           },
           openGraph:{
-            title:decodeURIComponent(params.description.split('|')[0]),
-            title:decodeURIComponent(params.description.split('|')[0]),
+            title:decodeURIComponent(params.description),
             url:'https://gorillaworkout.space',
             siteName:'next.js',
             images:[{
-              url:decodeURIComponent(params.id),
+              url:decodeURIComponent(params.img),
               width:400,
               height:400
             }]
@@ -39,21 +38,22 @@ export default function Page({params}){
     return (
         <>
             <Head>
-                <title>CERTIFICATE {params.id}</title>
+                <title>CERTIFICATE {params.img}</title>
                 <meta name="description" content="this is from @gorillaworkout." />
                 {/* <meta property="og:image" content={MyPic} /> */}
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
             </Head>
-            <div className="w-screen h-screen flex flex-col justify-center items-center"> 
-                <h1>CERTIFICATE {params.id}, {decodeURIComponent(params.description)}</h1>
-                <h1>{decodeURIComponent(params.description).split('|')}</h1>
-                <h1>{decodeURIComponent(params.description).split('|')[0]}</h1>
+            <div className="w-screen h-screen flex flex-col justify-center items-center">
+            <h1>CERTIFICATE {params.img}, {decodeURIComponent(params.badge)}</h1> 
+                <h1>CERTIFICATE {params.img}, {decodeURIComponent(params.content)}</h1>
+                <h1>CERTIFICATE {params.img}, {decodeURIComponent(params.description)}</h1>
+                <h1>CERTIFICATE {params.img}, {decodeURIComponent(params.issue)}</h1>
                 {/* <h2>{decodeURIComponent(params.id)}</h2> */}
             </div>
             <div className="grid grid-cols-2 lg:py-28 lg:px-28 md:py-20 md:px-20 sm:px-14 sm:py-14 bg-white w-screen h-screen">
                     <div className="bg-white flex justify-center items-center p-5">
-                        <img src={decodeURIComponent(params.id)} alt="" className="w-full h-full rounded-md" />
+                        <img src={decodeURIComponent(params.img)} alt="" className="w-full h-full rounded-md" />
                     </div>
                     <div className="bg-white flex flex-col items-start justify-start p-5">
                         <h1 className="text-[#DA4A4A] text-[30px] lg:mb-10">Badges Information</h1>
@@ -62,7 +62,7 @@ export default function Page({params}){
                                 BADGE ID
                             </p>
                             <p className="text-black text-[30px] font-semibold leading-10 ">
-                                {decodeURIComponent(params.description.split('|')[0])}
+                                {decodeURIComponent(params.badge)}
                             </p>
                         </div>
                         <div className="flex flex-col mb-5">
@@ -70,7 +70,7 @@ export default function Page({params}){
                                 Content Name
                             </p>
                             <p className="text-[#DA4A4A] text-[30px] font-semibold leading-10 ">
-                            {decodeURIComponent(params.description.split('|')[0])}
+                            {decodeURIComponent(params.content)}
                             </p>
                         </div>
                         <div className="flex flex-col mb-5">
@@ -78,7 +78,7 @@ export default function Page({params}){
                                 Description
                             </p>
                             <p className="text-[#282828] text-[30px] font-semibold leading-10 ">
-                            {decodeURIComponent(params.description.split('|')[0])}
+                            {decodeURIComponent(params.description)}
                             </p>
                         </div>
                         <div className="flex flex-col mb-5">
@@ -86,7 +86,7 @@ export default function Page({params}){
                                 ISSUE DATE
                             </p>
                             <p className="text-[#282828] text-[30px] font-semibold leading-10 ">
-                            {decodeURIComponent(params.description.split('|')[0])}
+                            {decodeURIComponent(params.issue)}
                             </p>
                         </div>
                     </div>  
